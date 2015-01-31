71802bd40edfb3cce8affcb335618466924312a1
Trevor Smith <TrevorSmith1275@gmail.com>
2015-01-31 14:37:06 -0500

    added the Monster class

--- Gameplay.py
+++ Gameplay.py
@@ -48,6 +48,32 @@ class Character(object):
 #TBD - import Character    
 from random import randint  
     
+class Monster(object):
+  def __init__(self, name, level, description, bias, undead, plant, speed
+               enhancement_method, fight_method,
+               chase_method,
+               bad_stuff_description, bad_stuff_method,
+               good_stuff_method, level_rewarded, treasure_rewarded):
+    self.name = name
+    self.level = level
+    self.description = description
+    self.bias = bias
+    self.undead = undead
+    self.plant = plant
+    self.speed = speed
+    
+    self.enhancement_method = enhancement_method
+    self.fight_method = fight_method
+    
+    self.chase_method = chase_method
+    
+    self.bad_stuff_description = bad_stuff_description
+    self.bad_stuff_method = bad_stuff_method
+
+    self.good_stuff_method = good_stuff_method
+    self.level_rewarded = level_rewarded
+    self.treasure_rewarded = treasure_rewarded
+
 
 #Runs gameplay
 
