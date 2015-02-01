4fd62074a91ada7492c3778fde328b785e8fe974
Trevor Smith <TrevorSmith1275@gmail.com>
2015-02-01 11:51:28 -0500

    Added more monsters and 1 attribute
    -Added Drooling Slime, Pit bull, Flying Frogs, Large Angry Chicken, Mr.
    Bones, and Gelatinous Octahedron
    
    - Added self.level attribute

--- Monster_Pokedex.py
+++ Monster_Pokedex.py
@@ -8,6 +8,7 @@ class Maul_Rat(Monster):
     def __init__(self):
         self.type = "monster"
         self.name = "Maul Rat"
+        self.level = 1
         self.description = "A homely-looking rat wearing a wig and wielding a mallet. A creature from Hell. +3 against Clerics."
         self.undead = False
         self.plant = False
@@ -32,6 +33,7 @@ class Crabs(Monster):
     def __init__(self):
         self.type = "monster"
         self.name = "Crabs"
+        self.level = 1
         self.description = "Not the sea creature. It cannot be Outrun"
         self.undead = False
         self.plant = False
@@ -50,6 +52,7 @@ class Potted_Plant(Monster):
     def __init__(self):
         self.type = "monster"
         self.name = "Potted Plant"
+        self.level = 1
         self.description = "A potted plant... thats it. Elves gain an extra Treasure after defeating it."
         self.undead = False
         self.plant = True
@@ -68,6 +71,7 @@ class Lame_Goblin(Monster):
     def __init__(self):
         self.type = "monster"
         self.name = "Lame Goblin"
+        self.level = 1
         self.description = "A small goblin limping with a crutch. +1 to Run Away"
         self.undead = False
         self.plant = False
@@ -81,6 +85,124 @@ class Lame_Goblin(Monster):
         self.enhancement = pass
         self.bias = pass
         self.good_stuff = pass
+
+class Drooling_Slime(Monster):
+    def __init__(self):
+        self.type = "monster"
+        self.name = "Drooling Slime"
+        self.level = 1
+        self.description = "Yucky slime! +4 against Elves."
+        self.undead = False
+        self.plant = False
+        self.speed = 0
+        self.level_rewarded = 1
+        self.treasure_rewarded = 1
+        self.bad_stuff_description = "Discard the Footgear you are wearing. Lose a level if you are not wearing any Footgear."
+        self.bad_stuff = lose_level(1)
+        self.fight = pass
+        self.chase = pass
+        self.enhancement = pass
+        self.bias = elves(4, battle_dict)
+        self.good_stuff = pass
+
+########## Level 2 ##########
+
+class Pit_Bull(Monster):
+    def __init__(self):
+        self.type = "monster"
+        self.name = "Pit Bull"
+        self.level = 2
+        self.description = "If you can't defeat it, you may distract it (automatic escape) by dropping any wand, pole, or staff. (Fetch, Fido!)"
+        self.undead = False
+        self.plant = False
+        self.speed = 0
+        self.level_rewarded = 1
+        self.treasure_rewarded = 1
+        self.bad_stuff_description = "Fang marks in your butt. Lose 2 levels."
+        self.bad_stuff = lose_level(2)
+        self.fight = bribe(wand, pole, staff)#we will have to add a boelean attribute to items classes for this
+        self.chase = pass
+        self.enhancement = pass
+        self.bias = pass
+        self.good_stuff = pass
+
+class Flying_Frogs(Monster):
+    def __init__(self):
+        self.type = "monster"
+        self.name = "Flying Frogs"
+        self.level = 2
+        self.description = "-1 to Run Away"
+        self.undead = False
+        self.plant = False
+        self.speed = 1
+        self.level_rewarded = 1
+        self.treasure_rewarded = 1
+        self.bad_stuff_description = "They bite!. Lose 2 levels."
+        self.bad_stuff = lose_level(2)
+        self.fight = pass
+        self.chase = pass
+        self.enhancement = pass
+        self.bias = pass
+        self.good_stuff = pass
+
+class Mr_Bones(Monster):
+    def __init__(self):
+        self.type = "monster"
+        self.name = "Mr. Bones
+        self.level = 2
+        self.description = "A skeleton dancing in a top hat. If you must flee you lose a level even if you escape."
+        self.undead = True
+        self.plant = False
+        self.speed = 0
+        self.level_rewarded = 1
+        self.treasure_rewarded = 1
+        self.bad_stuff_description = "His bony touch costs you 2 levels."
+        self.bad_stuff = lose_level(2)
+        self.fight = pass
+        self.chase = lose_level(1)
+        self.enhancement = pass
+        self.bias = pass
+        self.good_stuff = pass
+
+class Large_Angry_Chicken(Monster):
+    def __init__(self):
+        self.type = "monster"
+        self.name = "Large Angry Chicken"
+        self.level = 2
+        self.description = "Fried Chicken is delicious. Gain an exrtra level if you defeat it with fire or flame."
+        self.undead = False
+        self.plant = False
+        self.speed = 0
+        self.level_rewarded = 1
+        self.treasure_rewarded = 1
+        self.bad_stuff_description = "Very painful pecking. Lose a level."
+        self.bad_stuff = lose_level(1)
+        self.fight = used_fire(#checks if item or one shot in battle dictionary has fire attribute)
+        self.chase = pass
+        self.enhancement = pass
+        self.bias = pass
+        self.good_stuff = pass
+
+class Gelatinous_Octahedron(Monster):
+    def __init__(self):
+        self.type = "monster"
+        self.name = "Gelatinous Octahedron"
+        self.level = 2
+        self.description = "+1 to Run Away"
+        self.undead = False
+        self.plant = False
+        self.speed = -1
+        self.level_rewarded = 1
+        self.treasure_rewarded = 1
+        self.bad_stuff_description = "Drop all your Big items."
+        self.bad_stuff = lose_big_items()
+        self.fight = pass
+        self.chase = pass
+        self.enhancement = pass
+        self.bias = pass
+        self.good_stuff = pass
+
+
 
 
 
