c5472fc553ffa1576e30d2c18c0b38dd1dc7e5e0
APatton1247 <apatton1247@gmail.com>
2015-04-09 19:04:17 -0400

    Euler Problem 3 comments
    Added comments

--- Euler Questions/Problem 3.py
+++ Euler Questions/Problem 3.py
@@ -8,6 +8,14 @@ def prime(n):
   if n < 2:
     isPrime = False
   for i in range(3, int(math.sqrt(n)), 2):
+    #The divisibility of n by i says more about the
+    # prime-ness of n than it does about the prime-ness
+    # of i.  If n is divisible by any of the numbers in
+    # the above range, then n is not prime, but it
+    # doesn't follow that i is prime if the division is
+    # not even. You'll need to perform a different test
+    # on each number in the range (nice job skipping the
+    # even numbers, btw) to test their prime-ness.
     if n % i == 0:
       isPrime = False
     else:
@@ -21,6 +29,7 @@ def factors(n):
   for i in primes:
     if n % i == 0:
       prime_factors.append(i)
+  #I think this should be print(prime_factors)
   print (factors)
 
 #finds highest factor in factor list
