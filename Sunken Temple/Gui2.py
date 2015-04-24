bc11fb7a9e5da8947afeb3c2ea06eaafb999bdd7
APatton1247 <apatton1247@gmail.com>
2015-04-23 21:00:40 -0400

    GUI look & feel
    Made bg image with stone walls and obelisk.  Chg size, shape, font, and
    location of label and entry.

--- Sunken Temple/Gui2.py
+++ Sunken Temple/Gui2.py
@@ -10,15 +10,18 @@ class Gui(Frame):
         self.master.title("Sunken Temple")
 
     def create_widgets(self):
-        bg_canvas = Canvas(self.master, height = 590, width = 1180)
+        win_height = 900
+        win_width = 1536
+        bg_canvas = Canvas(self.master, height = win_height, width = win_width)
         bg_canvas.pack(expand = True, fill = "both")
-        wall = PhotoImage(file = "StoneWall.gif")
+        wall = PhotoImage(file = "gui_bg_1.gif")
         bg_canvas.img = wall
         bg_canvas.create_image(0, 0, anchor = "nw", image = wall)
-        entry = Entry(self.master, width = 80)
-        output_label = Label(self.master, bg = "white", anchor = "nw", width = 80, height = 20)
-        bg_canvas.create_window(300, 100, anchor = "nw", window = output_label)
-        bg_canvas.create_window(340, 500, anchor = "nw", window = entry)
+        entry = Entry(self.master, width = 60, font=("Arial", 20))
+        output_label = Label(self.master, bg = "white", anchor = "nw", width = 60, height = 18, font=("Arial", 20))
+        output_label.config(text = "This is the sample text!\nHello World!")
+        bg_canvas.create_window((win_width//7)+40, (win_height//8), anchor = "nw", window = output_label)
+        bg_canvas.create_window((win_width//7)+70, (6*win_height//7), anchor = "nw", window = entry)
         
 
 gui = Gui(root)
