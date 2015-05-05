f47db13a87a91b16396f74d303842260a82e9a0a
Trevor Smith <TrevorSmith1275@gmail.com>
2015-05-05 19:05:42 -0400

    Input output entry connection thing

--- Sunken Temple/Gui2.py
+++ Sunken Temple/Gui2.py
@@ -1,4 +1,5 @@
 from tkinter import *
+#import Introduction as intro
 
 root = Tk()
 
@@ -9,6 +10,10 @@ class Gui(Frame):
         self.create_widgets()
         self.master.title("Sunken Temple")
 
+    def key(self, event):
+        self.output_text.set(self.output_text.get() + "\n" + self.input_text.get())
+        self.input_text.set("")
+
     def create_widgets(self):
         win_height = 900
         win_width = 1536
@@ -17,12 +22,18 @@ class Gui(Frame):
         wall = PhotoImage(file = "gui_bg_1.gif")
         bg_canvas.img = wall
         bg_canvas.create_image(0, 0, anchor = "nw", image = wall)
-        entry = Entry(self.master, width = 60, font=("Arial", 20))
+        self.input_text = StringVar()
+        self.input_text.set("")
+        entry = Entry(self.master, width = 60, font=("Arial", 20), text = self.input_text)
+        entry.bind("<Return>", self.key)
+        self.output_text = StringVar()
+        self.output_text.set("This is the sample text!\rHello World!")
         output_label = Label(self.master, bg = "white", anchor = "nw", width = 60, height = 18, font=("Arial", 20))
-        output_label.config(text = "This is the sample text!\nHello World!")
-        bg_canvas.create_window((win_width//7)+40, (win_height//8), anchor = "nw", window = output_label)
-        bg_canvas.create_window((win_width//7)+70, (6*win_height//7), anchor = "nw", window = entry)
-        
+        output_label.config(textvariable = self.output_text, justify = "left")
+        bg_canvas.create_window((win_width//4)+20, (win_height//8), anchor = "nw", window = output_label)
+        bg_canvas.create_window((win_width//4)+20, (6.5*win_height//10), anchor = "nw", window = entry)
+
+            
 
 gui = Gui(root)
 root.mainloop()
