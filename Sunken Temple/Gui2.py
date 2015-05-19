aae4ed6f10345375f236d42f84bf03c29579c881
Trevor Smith <TrevorSmith1275@gmail.com>
2015-05-19 18:15:39 -0400

    Player stats label

--- Sunken Temple/Gui2.py
+++ Sunken Temple/Gui2.py
@@ -19,21 +19,25 @@ class Gui(Frame):
         win_width = 1536
         bg_canvas = Canvas(self.master, height = win_height, width = win_width)
         bg_canvas.pack(expand = True, fill = "both")
+
         wall = PhotoImage(file = "gui_bg_1.gif")
         bg_canvas.img = wall
         bg_canvas.create_image(0, 0, anchor = "nw", image = wall)
+
         self.input_text = StringVar()
         self.input_text.set("")
-        entry = Entry(self.master, width = 60, font=("Arial", 20), text = self.input_text)
+        entry = Entry(self.master, bg = "gray", width = 60, font=("Arial", 20), text = self.input_text)
         entry.bind("<Return>", self.key)
+
         self.output_text = StringVar()
         self.output_text.set("This is the sample text!\rHello World!")
-        output_label = Label(self.master, bg = "white", anchor = "nw", width = 60, height = 18, font=("Arial", 20))
+        output_label = Label(self.master, bg = "gray", anchor = "nw", width = 60, height = 18, font=("Arial", 20))
         output_label.config(textvariable = self.output_text, justify = "left")
+
+        player_stats_label = Label(self.master, bg = "gray", anchor ="nw", width = 20, height = 6, font=("Arial",20))
+
         bg_canvas.create_window((win_width//4)+20, (win_height//8), anchor = "nw", window = output_label)
         bg_canvas.create_window((win_width//4)+20, (6.5*win_height//10), anchor = "nw", window = entry)
-
-            
 
 gui = Gui(root)
 root.mainloop()
