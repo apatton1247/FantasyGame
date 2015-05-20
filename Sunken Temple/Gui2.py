88078164a4380f313600886ce01a2ddd154f4493
apatton1247 <apatton1247@gmail.com>
2015-05-19 22:57:16 -0400

    Fixed write()
    Renamed key() to write() (hopefully to help future incorporation of easy
    printing), now takes text from the Entry widget and copies it to the
    Label widget, and blanks the Entry line again.

--- Sunken Temple/Gui2.py
+++ Sunken Temple/Gui2.py
@@ -10,8 +10,12 @@ class Gui(Frame):
         self.create_widgets()
         self.master.title("Sunken Temple")
 
-    def key(self, event):
-        self.output_text.set(self.output_text.get() + "\n" + self.input_text.get())
+    def write(self, event, *text):
+        if text:
+            for line in text:
+                self.output_text.set(self.output_text.get() + line)
+        else:
+            self.output_text.set(self.output_text.get() + "\n" + self.input_text.get())
         self.input_text.set("")
 
     def create_widgets(self):
@@ -26,20 +30,18 @@ class Gui(Frame):
 
         self.input_text = StringVar()
         self.input_text.set("")
-        entry = Entry(self.master, bg = "gray", width = 60, font=("Arial", 20), text = self.input_text)
-        entry.bind("<Return>", self.key)
+        entry = Entry(self.master, bg = "gray", width = 60, font=("Arial", 16), textvariable = self.input_text)
+        entry.bind("<Return>", self.write)
 
         self.output_text = StringVar()
         self.output_text.set("This is the sample text!\rHello World!")
-        output_label = Label(self.master, bg = "gray", anchor = "nw", width = 60, height = 18, font=("Arial", 20))
+        output_label = Label(self.master, bg = "gray", anchor = "nw", width = 60, height = 18, font=("Arial", 16))
         output_label.config(textvariable = self.output_text, justify = "left")
 
-        player_stats_label = Label(self.master, bg = "gray", anchor ="nw", width = 20, height = 6, font=("Arial",20))
+        player_stats_label = Label(self.master, bg = "gray", anchor ="nw", width = 20, height = 6, font=("Arial",16))
 
         bg_canvas.create_window((win_width//4)+20, (win_height//8), anchor = "nw", window = output_label)
         bg_canvas.create_window((win_width//4)+20, (6.5*win_height//10), anchor = "nw", window = entry)
 
 gui = Gui(root)
 root.mainloop()
-#intro.run()
-#print("Done")
