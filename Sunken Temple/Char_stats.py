9962cff641e0b1592e89648ffd77b0d10d071b67
APatton1247 <apatton1247@gmail.com>
2015-06-05 21:46:39 -0400

    Attributes pie chart update
    Included some new arguments to the pie plot and legend constructors,
    such as legend transparency & location, and some explanation of autopct
    & pctdistance.
    Added legend formatting function.

--- Sunken Temple/Char_stats.py
+++ Sunken Temple/Char_stats.py
@@ -8,13 +8,30 @@ labels = ['Strength', 'Spirit', 'Intellect']
 values = [num_strength, num_spirit, num_intellect]
 colors = ['FireBrick', 'Khaki', 'SteelBlue']
 
+
 def my_autopct(pct):
     total=sum(values)
     val=int(pct*total/100.0)
     return '{v:d}'.format(v=val)
+
+def format_labels(labels, values):
+    lab_and_val = zip(labels, values)
+    lab_val_strings = []
+    for pair in lab_and_val:
+        new_pair = pair[0] + ": " + str(pair[1])
+        lab_val_strings.append(new_pair)
+    return lab_val_strings
   
-plt.pie(values, colors=colors, autopct= my_autopct, shadow=True, startangle=90)
-plt.legend(title="Attributes", labels=labels, loc='lower right')
+plt.pie(values, colors=colors, shadow=True,
+        #When autopct is specified, internal labels show at a radius of
+        #pctdistance away from the center of the graph.  Uncomment them to see!
+        #autopct= my_autopct, pctdistance = 1.2,
+        startangle=90)
+plt.legend(title="Attributes", labels= format_labels(labels, values),
+        #framealpha sets the transparency of the legend
+        framealpha = 0, loc=(.8, -.1))
+        #This loc form can also be used, looks about the same on my screen:
+        #loc = "lower right", bbox_to_anchor = (1.1, -0.1))
 
 plt.axis('equal')
 plt.show()
