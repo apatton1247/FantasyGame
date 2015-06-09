c91e655eaab8355519b4e4a7a155531518afa237
Trevor Smith <TrevorSmith1275@gmail.com>
2015-06-09 19:46:14 -0400

    Importing matplotlib

--- Sunken Temple/Gui.py
+++ Sunken Temple/Gui.py
@@ -1,3 +1,7 @@
+import matplotlib
+matplotlib.use('TkAgg')
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2TkAgg
+from matplotlib.figure import Figure
 from tkinter import *
 #import Gameplay as game
 
