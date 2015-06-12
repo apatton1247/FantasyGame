418e5ddc4a280aeff20a85b0fd545b76a80cfe21
Trevor Smith <TrevorSmith1275@gmail.com>
2015-06-11 21:37:15 -0400

    Barchart

--- Sunken Temple/Char_stats.py
+++ Sunken Temple/Char_stats.py
@@ -4,34 +4,21 @@ num_strength = 15
 num_spirit = 2
 num_intellect = 5
 
-labels = ['Strength', 'Spirit', 'Intellect']
-values = [num_strength, num_spirit, num_intellect]
-colors = ['FireBrick', 'Khaki', 'SteelBlue']
+pie_labels = ['Strength', 'Spirit', 'Intellect']
+pie_values = [num_strength, num_spirit, num_intellect]
+pie_colors = ['FireBrick', 'Khaki', 'SteelBlue']
 
 
-def my_autopct(pct):
-    total=sum(values)
-    val=int(pct*total/100.0)
-    return '{v:d}'.format(v=val)
-
-def format_labels(labels, values):
-    lab_and_val = zip(labels, values)
+def format_labels(pie_labels, pie_values):
+    lab_and_val = zip(pie_labels, pie_values)
     lab_val_strings = []
     for pair in lab_and_val:
         new_pair = pair[0] + ": " + str(pair[1])
         lab_val_strings.append(new_pair)
     return lab_val_strings
   
-plt.pie(values, colors=colors, shadow=True,
-        #When autopct is specified, internal labels show at a radius of
-        #pctdistance away from the center of the graph.  Uncomment them to see!
-        #autopct= my_autopct, pctdistance = 1.2,
-        startangle=90)
-plt.legend(title="Attributes", labels= format_labels(labels, values),
-        #framealpha sets the transparency of the legend
-        framealpha = 0, loc=(.8, -.1))
-        #This loc form can also be used, looks about the same on my screen:
-        #loc = "lower right", bbox_to_anchor = (1.1, -0.1))
+plt.pie(pie_values, colors=pie_colors, shadow=True, startangle=90)
+plt.legend(title="Attributes", labels=format_labels(pie_labels, pie_values), framealpha = 0, loc=(.8, -.1))
 
 plt.axis('equal')
 plt.show()
