e30c0f55ce46459dea09f9838573b6bb6a6cea2a
Trevor Smith <TrevorSmith1275@gmail.com>
2015-06-12 18:25:14 -0400

    Finished Xp bar chart

--- Sunken Temple/Xp_barchart.py
+++ Sunken Temple/Xp_barchart.py
@@ -1,16 +1,32 @@
-from pylab import *
+import matplotlib.pyplot as plt; plt.rcdefaults()
+import numpy as np
+import matplotlib.pyplot as plt
 
+#variables
 char_xp = 250
-level_xp = 300 #for example max 300xp for level 6
+level_xp = 300
 
-pos = arange(1)
+y_pos = np.arange(1)
+#This says there is only one bar
+fig = plt.figure()
+#Something to do with turning the plot into a figure?
+ax = fig.add_subplot(111)
+#Defining ax and positioning the subplot??
+ax.barh(y_pos, char_xp, color='lime')
+#Basic construction of the bar chart
 
-fig = barh(pos, (char_xp), align = "center", color= "green")
-plt.ylabel('Xp', rotation=90)
-
-plt.tick_params(axis='y',which='both',left='off',right='off',labelleft='off')
-
+plt.tick_params(axis='y', which='both', left='off', right='off',labelleft='off')
+#Removes the left and right ticks and default tick labels
+plt.ylabel("Xp", rotation='horizontal', fontsize=18, verticalalignment='center')
+#Defines the y axis label and rotates
+ax.yaxis.set_label_coords(1.04,.65)
+#Moves y label to the correct position
 plt.xlim(0,level_xp)
-subplots_adjust(top=.15)
+#Sets limits on the x axis
+plt.ylim(0,.1)
+#set limits on the y axis
+plt.subplots_adjust(top=.15)
+#Decreases width of entire chart
 
-show()
+
+plt.show()
