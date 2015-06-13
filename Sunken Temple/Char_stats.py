0d87614ff0beec35a59017b3e6422b4d1e7df82a
Trevor Smith <TrevorSmith1275@gmail.com>
2015-06-12 20:08:09 -0400

    updated pie chart

--- Sunken Temple/Char_stats.py
+++ Sunken Temple/Char_stats.py
@@ -8,7 +8,7 @@ import tkinter as tk
 num_strength = 15
 num_spirit = 2
 num_intellect = 5
-
+char_battle_strength = 25
 pie_labels = ['Strength', 'Spirit', 'Intellect']
 pie_values = [num_strength, num_spirit, num_intellect]
 pie_colors = ['FireBrick', 'Khaki', 'SteelBlue']
@@ -32,11 +32,14 @@ frame = tk.Frame(master = root)
 frame.pack(fill = "both", expand = True)
 test_label = tk.Label(frame, width = 150, height = 2, text = "Hello!")
 test_label.pack(side = "top")
-f = plt.Figure(figsize = (8, 8), dpi = 100)
-piechart = f.add_subplot(111)
-piechart.pie(values, colors = colors, shadow = True, startangle = 90)
-piechart.legend(title="Attributes", labels= format_labels(labels, values), framealpha = 0, loc=(.8, -.1))
-canvas = FigureCanvasTkAgg(f, frame)
+fig = plt.Figure(figsize = (8, 8), dpi = 100)
+piechart = fig.add_subplot(111)
+piechart.pie(pie_values, colors = pie_colors, startangle = 90)
+legend = piechart.legend(title="Attributes", labels= format_labels(pie_labels, pie_values)
+                         ,framealpha = 0, loc=(.84, .01), fontsize=16)
+plt.setp(legend.get_title(),fontsize=20)
+piechart.text(1.2, .5, 'BS', bbox=dict(facecolor='none',pad=20), fontsize=80)
+canvas = FigureCanvasTkAgg(fig, frame)
 canvas.show()
 canvas.get_tk_widget().pack(side = tk.TOP, fill = tk.BOTH, expand = True)
 canvas._tkcanvas.pack(side=tk.TOP, fill=tk.BOTH, expand=1)
