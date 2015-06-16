250cd9489e908f3779f35f62bf58be17580317f8
APatton1247 <apatton1247@gmail.com>
2015-06-15 22:57:57 -0400

    Began adding Pie Chart to Stats

--- Sunken Temple/Gui.py
+++ Sunken Temple/Gui.py
@@ -3,6 +3,7 @@ matplotlib.use('TkAgg')
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2TkAgg
 from matplotlib.figure import Figure
 from matplotlib.ticker import MultipleLocator
+from matplotlib.font_manager import FontProperties
 #import matplotlib.pyplot as plt
 
 from tkinter import *
@@ -34,6 +35,14 @@ class Gui(Tk):
             self.output_text.set(self.output_text.get() + "\n" + self.input_text.get())
         self.input_text.set("")
 
+    def format_labels(self, pie_labels, pie_values):
+        lab_and_val = zip(pie_labels, pie_values)
+        lab_val_strings = []
+        for pair in lab_and_val:
+            new_pair = pair[0] + ": " + str(pair[1])
+            lab_val_strings.append(new_pair)
+        return lab_val_strings
+    
     def char_stats(self):
         #For future implementation
         #char_stats_stack = Frame(self.root)
@@ -60,7 +69,7 @@ class Gui(Tk):
         p_level = 7
         p_color = "darkviolet"
         level_fig = Figure()
-        level_bar = level_fig.add_subplot(111)
+        level_bar = level_fig.add_subplot(171)
         level_bar.set_title(level_bar.get_title(), text = "Level", fontsize = 20)
         level_bar.bar(0, p_level, width = .1, color = p_color)
         level_bar.yaxis.set_major_locator(MultipleLocator(1))
@@ -68,11 +77,34 @@ class Gui(Tk):
         level_bar.set_ylim(1, 11)
         level_bar.set_xlim(0, 0.1)
         level_bar.tick_params(axis = "x", top = "off", bottom = "off", labelbottom = "off")
-        level_fig.subplots_adjust(right=.25)
+        #level_fig.subplots_adjust(right=.25)
         level_canvas = FigureCanvasTkAgg(level_fig, mid_frame)
         level_canvas.show()
         level_canvas.get_tk_widget().pack(side = "left")
-#TODO: Add the pie chart and xp chart        
+
+        pie_strength = 15
+        pie_spirit = 2
+        pie_intellect = 5
+        char_battle_strength = 25
+        pie_labels = ['Strength', 'Spirit', 'Intellect']
+        pie_values = [pie_strength, pie_spirit, pie_intellect]
+        pie_colors = ['FireBrick', 'Khaki', 'SteelBlue']
+        #pie_fig = Figure()
+        pie_chart = level_fig.add_subplot(122)
+        pie_chart.pie(pie_values, colors = pie_colors, startangle = 90)
+        pie_chart.axis("equal")
+        pie_legend = pie_chart.legend(title="Attributes", labels= self.format_labels(pie_labels, pie_values)
+                         ,framealpha = 0, loc=(.82, .01), fontsize=16)
+        pie_legend.set_title(pie_legend.get_title(), prop = FontProperties(size = 20))
+        pie_chart.text(1.14, .5, str(char_battle_strength), bbox = dict(facecolor="none", pad=20), fontsize = 80)
+        #pie_canvas = FigureCanvasTkAgg(level_fig, mid_frame)
+        #pie_canvas.show()
+        #pie_canvas.get_tk_widget().pack(side = "left")
+#TODO: Add the xp chart        
+
+        
+        #Maybe figure out if instead of using subplots_adjust() we can just modify the overall
+        #figure by adding more subplots, updating the call add_subplot(val1, val2, val3, etc)
 
 #TODO: Once we have multiple players's stats, this will return char_stats_stack
         return char_stats
@@ -105,12 +137,12 @@ class Gui(Tk):
         self.bg_canvas.create_window((win_width//4)+20, (win_height//8), anchor = "nw", window = output_label)
         self.bg_canvas.create_window((win_width//4)+20, (7*win_height//10), anchor = "nw", window = entry)
 
-        stats_frame = Frame(self.root, height = 250, width = 250)
-        stats_name = Label(stats_frame, width = 20, height = 2, justify = "left", anchor = "nw", bg = "light gray", font=("Arial",16), text = "Name\t   Status\nClass, Race", relief = "sunken")
-        stats_mid_frame = Frame(stats_frame)
-        stats_level_bar = Label(stats_mid_frame, width = 4, height = 6, justify = "left", anchor = "nw", bg = "light gray", font=("Arial",16), text = "L\ne\nv\ne\nl\n!", relief = "sunken")
-        stats_pie_chart = Label(stats_mid_frame, width = 16, height = 6, bg = "light gray", font=("Arial",16), text = "Pie Chart\ngoes here!", relief = "sunken")
-        stats_xp_bar = Label(stats_frame, width = 20, height = 2, bg = "light gray", font=("Arial",16), text = "Here is the\nXP bar!", relief = "sunken")
+#        stats_frame = Frame(self.root, height = 250, width = 250)
+#        stats_name = Label(stats_frame, width = 20, height = 2, justify = "left", anchor = "nw", bg = "light gray", font=("Arial",16), text = "Name\t   Status\nClass, Race", relief = "sunken")
+#        stats_mid_frame = Frame(stats_frame)
+#        stats_level_bar = Label(stats_mid_frame, width = 4, height = 6, justify = "left", anchor = "nw", bg = "light gray", font=("Arial",16), text = "L\ne\nv\ne\nl\n!", relief = "sunken")
+#        stats_pie_chart = Label(stats_mid_frame, width = 16, height = 6, bg = "light gray", font=("Arial",16), text = "Pie Chart\ngoes here!", relief = "sunken")
+#        stats_xp_bar = Label(stats_frame, width = 20, height = 2, bg = "light gray", font=("Arial",16), text = "Here is the\nXP bar!", relief = "sunken")
 
         char_stats_frame = self.char_stats()
         self.bg_canvas.create_window((8*win_width/10), (win_height/8), anchor = "nw", window = char_stats_frame)
